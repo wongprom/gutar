d9bb80c4d1a9f53c839af56dccd4eea968bd3a06
wongprom <jimmybackstrom@hotmail.com>
2019-06-12 11:07:36 +0200

    Add 10 aricles as fakeData.

---
+++ src/utilities/fakeData.js
@@ -0,0 +1,107 @@
+import ArticleImage from "../images/sunset-1920x1079jpg.jpg";
+import AngBatar from "../images/gotlandsAngBatarAB.jpg";
+import Petterkvist from "../images/petterkvist.jpg";
+export const articles = [
+  {
+    id: 1,
+    img: ArticleImage,
+    heading: "Gotlands smedmästareförening",
+    articleContent: "har hållit årsmöte i Visby. Års- och revisionsberättelserna föredrogos och ansvarsfrihet beviljades. Föreningen räknar f. n. 55 medlemmar. Till ordförande för ett år omvaldes smidesmästare Axel Pettersson, Visby. De två övriga styrelseledamöter som voro i tur att avgå omvaldes, nämligen smidesmästarna Axel Eklund, Etelhem, och Hilding Wesley, Vall. Till styrelsesuppleanter omvaldes smidesmästarna Henrik Andersson, Hallvards, och Valter Wesley, Roma. Revisorer blevo smidesmästarna Axel Pettersson Burs (nyvald) och H. Yttergren, Källunge, (omvald) med smidesmästare Gösta Lundberg, Väskinde, (omvald) som suppleant.Ordföranden förrättade parentation över framlidne medlemmen smidesmästare Axel Ericsson, Barlingbo.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "2 Februari 1943"
+    }
+  },
+  {
+    id: 2,
+    img: null,
+    heading: "Till kaptenen",
+    articleContent: "i Norra skånska infanteriregementet har utnämnts löjtnant S. T. B. Cronlund, förut vid Gotlands infanteriregemente.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Lördagen den 11 december 1937"
+    }
+  },
+  {
+    id: 3,
+    img: null,
+    heading: "Inryckningstider för värnpliktiga.",
+    articleContent: "den 14 jan.: Kustartilleritjänst (utom förut inryckta) för maskinafdelningen afsedda och inskrifna 1914 på vårenden 1 febr.: Sjötjänst för minmatroser och ”öfrigas”, inskrifna 1914 på våren och hösten; stationstjänst för vapenföra skrifbiträden, inskrifna 1914 på våren och hösten; för icke vapenföra skrifbiträden utom för i värnpliktslagen i 27: 2 B nämnda värnpliktiga (studenter och likställda) samt för bagare, skräddare och skomakare, samtliga inskrifna 1914 på hösten. Kustartilleritjänst för samtliga utom de för maskinafdelningen afsedda, inskrifna 1914 på våren; halfva antalet 1914 på hösten inskrifna vapenföra icke studenter och likstälda (återstående halfva antalet inrycker först den 10 nov.); halfva antalet af icke vapenföra, inskrifna 1914 på hösten (återstående halfva antalet inrycker den 30 juni se nedan;den 7 maj: Kustartilleritjänst; icke vapenföra, inskrifna 1914 på våren från Malmöhus norra, Blekinge, Jönköpings, Stockholms och Gäfleborgs inskrifningsområden samt halfva antalet dito från Göteborgs och Bohus samt Gotlands inskrifningsområden;den 14 juni: Allmän tjänst för år 1915 inskrifna sjökaptener, styrmän, maskinister af 1. kl. (öfvermaskinister) och maskinister af 2. kl. Stationstjänst för läkare, inskrifna 1912, (repetitionsöfning); för värnpliktiga ingenjörer, intendentur, läkare, tekniker och elektrotekniker samt i värnpliktalagen §27; 2 B nämda (icke vapenföra studenter och likstälds) uttagna till skrifbiträden, samtliga inskrifna 1914 på hösten och 1915; den 26 juni: Sjötjänst för minmatroser, inskrifna 1912, (repetitionsöfning);den 28 juni: Kustartilleritjänst för studenter och likstälda, inskrifna 1914 på hösten och 1915;den 30 juni: Kustartilleritjänst för halfva antalet (förut icke inryckta) icke vapenförs, inskrifna 1914 på hösten.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Torsdagen 7 januari 1915"
+    }
+  },
+  {
+    id: 4,
+    img: AngBatar,
+    heading: "Ångfartygsaktlebolaget Gotlands ångbåtar.",
+    articleContent: "",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Fredagen den 3 juni 1932"
+    }
+  },
+  {
+    id: 5,
+    img: null,
+    heading: "Ung flicka försvunnen under sjöresan",
+    articleContent: "Då ångfartyget Drotten i morse anlände hit anmäldes till polisen, att 27-åriga fröken Agneta Alkvist, bördig från Wormsö, Estland, o. senast boende Banergatan 18, Stockholm, under resan försvunnit från båten och kunde antagas ha gått överbord och omkommit. Fröken Alkvist hade legat i en andraklasshytt på mellandäck, och vid båtens ankomst hit funnos hennes kläder och handväska kvar i hytten, medan hon själv som nämnts försvunnit. Ingen hade heller observerat, att -hon lämnat båten. De två medpassagerarna i hytten uppge, att den försvunna strax innan båten lämnade Nynäshamn klätt av sig men att hon icke gått och lagt sig, då hon förklarat sig lida av sömnlöshet. Hon lämnade därefter hytten iklädd nattdräkt och har icke vidare avhörts. Fröken Alkvists fästman, som var med på resan och fick reda på den tragiska händelsen vid båtens ankomst hit, har förklarat, att hon på senaste tiden förefallit nervös och nedslagen och varit oförmögen till arbete. Hon hade av läkare ordinerats medicin, men vägrat ta den, då hon var rädd för att den innehöll gift. Någon leda vid livet hade hon dock aldrig uttalat, men hon hade vid flera tillfällen uttryckt rädsla för att komma i fängelse. Vid resor med spårvagn i Stockholm hade det flera gånger hänt, att hon plötsligt hoppat av vagnen, då hon fått för sig, att den skulle gå direkt till Konradsbergs sjukhus. Fästmannens bestämda uppfattning är, att hon under resan hoppat i sjön och dränkt sig - förmodligen under ett anfall av sinnesförvirring. Resan hit företogs för att fröken Alkvist under någon tid skulle få vila upp hos en gift syster, som är bosatt i Fardhem. Själv har hon under flera år varit bosatt på Gotland, där hon bl. a. arbetat om hembiträde.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Tisdagen den 14 september 1943"
+    }
+  },
+  {
+    id: 6,
+    img: null,
+    heading: "Utländska sludenter campa i Visby nästa månad.",
+    articleContent: "Lund, 19 juni. (TT.) Enligt Lunds Dagblad kommer en tysk-engelskskandinavisk studentcamp att under tiden 17-24 juli hållas på Gotland. Antalet deltagare i lägrat kommer att begränsas till 15 tyskar, 15 engelsmän och 20 skandinaver. Dessutom beräknas ett antal framstående kulturpersonligheter lomma att närvara. Bland annat har från engelskt håll nämts John Masefveld och arkitekt Ei1i t samt från tyskt håll professorerna Rosenstock och Hans Feier.Lägret blir förlagt till den av Gotlands turistförening särskilt ordnade platsen omedelbart utanför ringmuren vid Galgberget.Programmet kommer att i huvudsak upptagas av debatter och gemensamma strövtåg på Gotland. Söndagen den 24 ämna de engelska studenterna ge ett ruinspel i Visby. Samma kväll sker avresan till Stockholm, där de engelska studenterna skola ge en föreställning på Skansen.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Måndagen den 20 juni 1932."
+    }
+  }, {
+    id: 7,
+    img: null,
+    heading: "Gotländska fynd lösa 1000-årig gåta.",
+    articleContent: "— Där offra de levande fåglar; runt omkring sticka de ner pilar i marken, andra också bröd och kött och vad var och en just har, som det är den härskande sedan hos dem. De kasta också lott om fåglarna, om de skola slakta dem eller uppäta dem eller låta dem vara i livet.För ungefär tusen år sen skrevs detta av en grekisk kejsare om en märklig fågelkult som utövades av svenskarna i Ryssland under deras färder på sina flottar nedför Dnjepr till Konstantinopel. Offret skedde vid en jättestor ek på en ö i Dnjepr, sedan resenärerna passerat de sju forsarna vid flodens nedre lopp.Det inte minst märkliga i saken är att vi äga fynd från Gotland, som utgör en arkeologisk parallell till denna skildring, enl. vad professor Birger Nerman berättar i senaste häft. av Fornvännen.Mellan de på ett avstånd av något mer än 1 kilometer från varandra belägna gårdarna Bjerges och Gudings i Vallstena ligga de s. k. Gudingsåkrarna. Fordom var här en sank löväng. Denna har sträckt sig söderut nedåt Källunge myr, och tydligen i samband med myrens utdikning har ängsmarken dränerats.Spjutspetsarna stuckos ner kring den heliga eken.Under långa tider ha åkrarna vid Gudings givit fynd, och enbart sedan 1920 ha hundratals föremål kommit i dagen, huvudsakligen järnvapen och bland dem till övervägande del spjutspetsar. Till största delen härröra fynden från tiden omkring år 800 och 800-talet, men även från 900-talet. Dr John Nihlén har tidigare konstaterat att en stenlagd forntida väg går fram över fyndplatsen. Fynden synas främst gruppera sig kring en stor, oregelbunden vattensamling, vars närmast angränsande. med buskage beväxta område ännu är ouppodlat. Nihlén utförde här grävningar, varvid en del typiska fynden gjordes. Fynden lågo i myll-lagrets bot t, u på gränsen till den underliggande sanden, och voro lätt skönjbara. Någon bestämd orientering av föremålen kunde ej iakttagas, men de flesta spjutspetsarna, där läget kunde iakttagas, lågo med spetsarna nedåt jorden. Föremålen, som voro begränsade till vissa stråk,ha lagts eller stuckits ner i jorden utefter en smal remsa på vissa smärre fläckar. I det med buskar beväxta området närmst vattensamlingen gjordes inga fynd.Att det här rör sig om offerfynd kan ej gärna dras i tvivelsmål. Detta framgår redan av fyndens fördelning och av att spjutspetsarna till stor del tydligt nedstuckits. Ängsmarkerna låga ju också nedåt Källunge myr och offer ha ju under forntiden ofta försiggått i eller intill myrar eller små insjöar.Här intresserar närmast ett bestämt fynd. Enligt uppgifter, som Nihlén erhöll, hade år 1920 ett stycke sydväst om vattensamlingen den dåvarande ägaren av Lilla Gudings gjort ett fynd som bestått av 8 stycken långa spjutspetsar. nedstuckna i jorden lodrätt med spetsarna ner och bildande en ring med cirka 2,5 meters diameter. Ejderhansblotarnas offerplats.Hos professor Nerman väcka de i den gotländska offerjorden nedstuckna spjutspetsarna tanken på de pilar som svenskarna i Ryssland stucko ned i jorden kring den heliga eken på S:t Gregot i Dnjepr. Spjutspetsrins diameter uppges ha varit c m., vilket mycket väl skulle passa om spjuten just stått nedstuckna runt ett träd i den löväng, som kan antagas ha funnits här redan under vikingatiden.Hittills ha tyvärr icke tillräckliga undersökningar ägt ruin på Gudingsåkrarna — en ingående utgrävning vore i hög grad önskvärd, anser prof. Nerman — och därför är det ej så lätt att säga vad det är för ett slags offer som bragts gudomen på denna kultplats.Att här förekommit fågeloffer av ??????????????????? Slutsatsen gör prof. Nerman av ortnamnet som betyder ejderhane. Ortnamnet Gudings är alltså uppkallat efter gudingarna, d., v. s. ejderhansblotarna, och betyder helt enkelt ejderhansblotarnas offerplats.Även i Eke finns en gård med namnet Gudings och även där har påträffats offerfynd, bestående av ett antal bronshalsringar från tiden omkring år 500. Alldeles intill befinner sig också här en liten myr.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Tisdagen den 12 januari 1943"
+    }
+  },
+  {
+    id: 8,
+    img: AngBatar,
+    heading: "Auktion hos Landtbrukaren Gunnar Gardell,",
+    articleContent: "Måndagen den 18 dennes kl. 11 f. m. hålles offentlig auktion hos Landtbrukaren Gunnar Gardell, Norrgårda i Vallstena, för försäljning af omkring 20,000 kg. rofvor, 7,000 kg. betmassa och ett mindre parti vårsädeshalm. Samlingen sker i Ösarfve myr.Anstånd till den 15 maj detta år.Bäl den 11 januari 1914.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Onsdagen 13 januari 1915"
+    }
+  },
+  {
+    id: 9,
+    img: ArticleImage,
+    heading: "En svår tjocka",
+    articleContent: "har under senaste dagarne rådt kring Gotlands kust, som orsakat att ångaren Klintehamn måste qvarligga i Fårösund, hvarifrån den hitkom först i går förmiddag kl. half 11 i stället för dagen förut på eftermiddagen. Fartyget afgick åter kl. 2 till hufvudstaden.Ångaren Gotland var härutanför på resa från Stockholm redan kl. 3 på morgonen i går, men inkom i hamnen ej förrän vid sextiden vägledd medels kanonskott och tutningar i lur från en lotsbåt, som utkommit mot ångaren.",
+    newsPaperObj ={
+      newsPaper: "Gotlands Allehanda",
+      date: "Onsdagen den 30 april 1884"
+    }
+  },
+  {
+    id: 10,
+    img: Petterkvist,
+    heading: "Petterkvist: Kolarbetaren på villovägar.",
+    articleContent: "",
+    newsPaperObj ={
+      newsPaper: "Gotlands Tidning",
+      date: "Onsdagen 2 Juni 1937"
+    }
+  },
+]
+
+
+
