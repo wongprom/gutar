eecacdb51eb0a4e375b868e78e48ed4c371c3d43
wongprom <jimmybackstrom@hotmail.com>
2019-06-17 12:12:05 +0200

    Add links and mixins in scss.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './Footer.scss';
 
+import { NavLink } from 'react-router-dom';
+
 const footer = () => {
   return (
     <footer>
-      <p>Footer</p>
-      <p>2019</p>
+      <div className="icons-wrapper">
+        <div className="icon-container"></div>
+        <div className="icon-container"></div>
+        <div className="icon-container"></div>
+      </div>
+      <div className="links-wrapper">
+        <div className="go-to">
+          <p className="sub-header">Gå till</p>
+          <ul>
+            <li>
+              <NavLink className="navBarNavigationItem" to="/">
+                Hem
+            </NavLink>
+            </li>
+            <li>
+              <NavLink className="navBarNavigationItem" to="/NyastArtiklar">
+                Nyast Artiklar
+            </NavLink>
+            </li>
+            <li>
+              <NavLink className="navBarNavigationItem" to="/MestLastaArtiklar">
+                Mest Lästa Artiklar
+            </NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+
     </footer>
   );
 };
