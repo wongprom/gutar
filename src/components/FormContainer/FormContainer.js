d8d3f2710387ebe298bea472d37a5a9130a89b98
wongprom <jimmybackstrom@hotmail.com>
2019-06-21 12:40:58 +0200

    Use one handler for all inputs.

--- src/components/FormContainer/FormContainer.js
+++ src/components/FormContainer/FormContainer.js
@@ -17,6 +17,7 @@ export class FormContainer extends Component {
   }
 
   formSubmitHandler = (e) => {
+    console.log("formSubmitHandler körs");
     //Submit form
     e.preventDefault();
   }
@@ -25,52 +26,56 @@ export class FormContainer extends Component {
     e.preventDefault();
   }
 
-  fullNameHandler = (e) => {
-    let value = e.target.value;
-    this.setState(prevState => ({
-      user:
-        { ...prevState.user, name: value }
-    }), () => console.log(this.state.user))
-  }
-
-  emailHandler = (e) => {
-    let value = e.target.value;
-    this.setState(prevState => ({
-      user:
-      {
-        ...prevState.user, email: value
-      }
-    }), () => console.log(this.state.user))
-  }
-
-  //Works for all inputs
-  // InputHandler = (e) => {
+  // fullNameHandler = (e) => {
   //   let value = e.target.value;
-  //   let name = e.target.name;
-  //   this.setState(prevState => {
-  //     return {
-  //       user:
-  //       {
-  //         ...prevState.user, email: value
-  //       }
-  //     }
-
-  //   }, () => console.log(this.state.user))
+  //   this.setState(prevState => ({
+  //     user:
+  //       { ...prevState.user, name: value }
+  //   }), () => console.log(this.state.user))
   // }
 
+  // emailHandler = (e) => {
+  //   let value = e.target.value;
+  //   this.setState(prevState => ({
+  //     user:
+  //     {
+  //       ...prevState.user, email: value
+  //     }
+  //   }), () => console.log(this.state.user))
+  // }
+  // textAreaHandler = (e) => {
+  //   let value = e.target.value;
+  //   this.setState(prevState => ({
+  //     user:
+  //     {
+  //       ...prevState.user, about: value
+  //     }
+  //   }), () => console.log(this.state.user))
+  // }
 
+  //Works for all inputs
+  InputHandler = (e) => {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(prevState => {
+      return {
+        user:
+        {
+          ...prevState.user, [name]: value
+        }
+      }
 
+    }, () => console.log(this.state.user))
+  }
 
   render() {
     let formTitle = {
       title: 'Kontakta Oss Via Email',
-      subText: '"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."',
+      subText: '"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC."',
     }
-
-
     return (
       <form>
-        <div className="container" onSubmit={this.formSubmitHandler}>
+        <div className="container">
           <h2>{formTitle.title}</h2>
           <p>{formTitle.subText}</p>
 
@@ -80,20 +85,33 @@ export class FormContainer extends Component {
             name={'name'}
             value={this.state.user.name}
             placeholder={'Namn'}
-            handleChange={this.emailHandler} /> {/* Name of the user */}
-
-
+            handleChange={this.InputHandler}
+          />
 
           <Input
             title={'E-post adress'}
             type={'text'}
-            name={'name'}
-            value={this.state.user.name}
+            name={'email'}
+            value={this.state.user.email}
             placeholder={'E-post adress'}
-            handleChange={this.fullNameHandler} />  {/* Input for Age */}
-          <TextArea />
+            handleChange={this.InputHandler}
+          />
+
+          <TextArea
+            title={'Din text'}
+            name={"about"}
+            rows={7}
+            cols={1}
+            value={this.state.user.about}
+            handleChange={this.InputHandler}
+            placeholder={"Din text"}
+          />
+
           <div className="btn-container">
-            <Button title={"Skicka"} />
+            <Button
+              title={"Skicka"}
+              action={this.formSubmitHandler}
+            />
             {/* <Button title={"Rensa"} /> */}
           </div>
         </div>
