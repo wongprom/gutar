43686871761bcd77637e2e06aa2a875597b11d12
wongprom <jimmybackstrom@hotmail.com>
2019-06-21 20:48:31 +0200

    Add text om heroImage.

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,6 +5,10 @@ import HeroImage from '../../images/sunset-1920x1079jpg.jpg';
 const heroSection = () => (
   <div className="hero-section">
     <img src={HeroImage} alt="test" />
+    <div className="text-box">
+      <h1>Gutar Förr</h1>
+      <p>Detta är bara en utfyllnadstext</p>
+    </div>
   </div>
 );
 
