f17e37662d00245005449d8f419ea9cf02a11df0
wongprom <jimmybackstrom@hotmail.com>
2019-06-23 15:51:17 +0200

    First commit. Add UI for MestLastaArtiklar page.

--- src/components/Pages/MestLastaArtiklar/MestLastaArtiklar.js
+++ src/components/Pages/MestLastaArtiklar/MestLastaArtiklar.js
@@ -1,10 +1,38 @@
-import React from 'react';
 
-const MestLastaArtiklar = () => {
+import React from 'react';
+import Divider from '../../Divider/Divider';
+import BigArticleCard from '../../Card/BigCard/BigArticleCard';
+
+import './MestLastaArtiklar.scss';
+
+import { articles } from "../../../utilities/fakeData";
+const MestLastaArtiklar = (props) => {
+
+  const data = articles
+  const header = 'Mest Lästa Artiklar';
+
+  const fixedDivider = {
+    className: "divider divider-fixed"
+  }
+
+  // if on page /MestLastaArtiklar, add fixed styling to component Divider.
+  const divider = props.match.path === "/MestLastaArtiklar" && <Divider className={fixedDivider.className} header={header} />
+
   return (
-    <div>
-      <p>MestLastaArtiklar</p>
+    <div className="mestLastaArtiklar">
+      {divider}
+      <main>
+        {/* OutPut amount of cards depending on data */}
+        {data.map(item =>
+          <BigArticleCard
+            heading={item.heading}
+            articleContent={item.articleContent}
+            img={item.img}
+          />
+        )}
+      </main>
     </div>
   );
 };
+
 export default MestLastaArtiklar;
