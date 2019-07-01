7ea84dd4d35ea1429c6dc5da957e2020a9944640
wongprom <jimmybackstrom@hotmail.com>
2019-07-01 18:57:18 +0200

    WIP. Structure kontakta oss page

--- src/components/Pages/KontaktaOss/KontaktaOss.js
+++ src/components/Pages/KontaktaOss/KontaktaOss.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Divider from '../../Divider/Divider'
 import FormContainer from '../../FormContainer/FormContainer';
+import contactUs from '../../../images/contact-us.jpg';
 
 import './KontaktaOss.scss';
 
-const KontaktaOss = () => {
+const KontaktaOss = (props) => {
+  const header = 'Kontakta Oss';
+  const fixedDivider = {
+    className: "divider divider-fixed"
+  }
+
+  const divider = props.match.path === "/KontaktaOss" && <Divider className={fixedDivider.className} header={header} />
+
+  console.log(props)
   return (
-    <div className="kontakta-oss" >
-      <FormContainer />
-    </div>
+
+    <div className="kontaktaOss" >
+      {divider}
+      <div id="header-text">
+        <p>Kontakta Oss</p>
+      </div>
+      <div id="header-image">
+        <img src={contactUs} />
+      </div>
+      <div className="formContainerWrapper">
+        <FormContainer />
+      </div>
+
+    </div >
+
+
   )
 }
 
