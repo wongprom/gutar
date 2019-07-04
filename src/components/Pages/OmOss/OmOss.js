cd64778368913f0e4acdbd0d96041cbbb2e96f05
wongprom <jimmybackstrom@hotmail.com>
2019-07-04 17:53:39 +0200

    First commit. WIP

--- src/components/Pages/OmOss/OmOss.js
+++ src/components/Pages/OmOss/OmOss.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./OmOss.scss";
 
 const OmOss = () => {
   return (
-    <div>
-      <p>OmOss</p>
+    <div id="om-oss">
+      <div className="text-box">
+        <h1>Om Oss</h1>
+        <p>“About” pages are an often-overlooked opportunity to distinguish yourself from your competitors and foster an appetite for your services. Misguided, thoughtless versions of these “About” pages are typically filled with bland, uninspiring imagery and information, which is doubly bad for someone in a creative industry. While working on your “About Me” or “About Us” page, consider it a chance to show off your creativity. You’re a designer; you don’t want to come off as a carbon copy, do you? Fortunately, this showcase offers up some quick inspiration, so check out these wonderful, highly-creative examples of appealing “About Me” pages.</p>
+      </div>
+      <div className="icon-box"></div>
     </div>
   );
 };
