82b3a7479ad00200270d89b521a3c9bf65951d9d
wongprom <jimmybackstrom@hotmail.com>
2019-07-05 21:35:40 +0200

    Structured OmOss page and made Divider component more dynamic.

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './Divider.scss';
 
 const divider = props => {
+
+  const fixedDivider = {
+    className: "divider-fixed"
+  }
+  // Use "divider" as className if not sended with props, otherwise there are no styling on Divider component.
+  const className = !props.className ? "divider divider-fixed" : props.className + ' ' + fixedDivider;
+  const header = !props.header ? "header={'Your Text Goes Here'}" : props.header;
+
+
   return (
-    <div className={props.className}>
+    <div className={className}>
       <p>
-        {props.header}
+        {header}
       </p>
     </div>
   );
--- src/components/Pages/OmOss/OmOss.js
+++ src/components/Pages/OmOss/OmOss.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import "./OmOss.scss";
+import Divider from '../../Divider/Divider';
 
-const OmOss = () => {
+const OmOss = (props) => {
+  console.log(props)
   return (
     <div id="om-oss">
-      <div className="text-box">
+      <Divider header={"Om Oss"} path={props.path} />
+      <div className="box-1">
         <h1>Om Oss</h1>
-        <p>“About” pages are an often-overlooked opportunity to distinguish yourself from your competitors and foster an appetite for your services. Misguided, thoughtless versions of these “About” pages are typically filled with bland, uninspiring imagery and information, which is doubly bad for someone in a creative industry. While working on your “About Me” or “About Us” page, consider it a chance to show off your creativity. You’re a designer; you don’t want to come off as a carbon copy, do you? Fortunately, this showcase offers up some quick inspiration, so check out these wonderful, highly-creative examples of appealing “About Me” pages.</p>
+        <div className="who">
+          <p>Who? My About me page needs to tell my readers who I am, and why that matters. When people see that I have years of experience, or that I juggle numerous blogs and hundreds of thousands of visitors, it translates into more credibility with my readers, and thus more stock in my advice.</p>
+        </div>
+        <div className="what">
+          <p>What? It's super important that my About page tells my readers exactly what I'm about. This is my call to action—offering to help my readers break into the blogosphere and start writing like a pro.</p>
+        </div>
+        <div className="why">
+          <p>Why? I want my readers to understand my stake in this process, and understand that I kicked off StartBloggingOnline.com in 2013 to address the needs I saw for some reliable, actionable advice.</p>
+        </div>
       </div>
-      <div className="icon-box"></div>
+      <div className="box-2">
+        <div className="inner-box">
+          <p>30K</p>
+          <p>Artiklar</p>
+        </div>
+        <div className="inner-box">
+          <p>1999</p>
+          <p>Kom första artikel</p>
+        </div>
+        <div className="inner-box">
+          <p>2017</p>
+          <p>lorem lorem</p>
+        </div>
+      </div>
     </div>
   );
 };
