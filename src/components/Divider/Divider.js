6e7fda049872688b5cc272fcf578ef47a7d935c9
wongprom <jimmybackstrom@hotmail.com>
2019-07-07 17:54:57 +0200

    DIVIDER. Add comments.

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -6,10 +6,9 @@ const divider = props => {
   const fixedDivider = {
     className: "divider-fixed"
   }
-  // Use "divider" as className if not sended with props, otherwise there are no styling on Divider component.
+  // default styling/text if not send by props.
   const className = !props.className ? "divider divider-fixed" : props.className + ' ' + fixedDivider;
   const header = !props.header ? "header={'Your Text Goes Here'}" : props.header;
-
 
   return (
     <div className={className}>
