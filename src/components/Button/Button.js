eb1a526f0cd878e0c81e481e855c4a50192727b0
wongprom <jimmybackstrom@hotmail.com>
2019-07-07 16:24:00 +0200

    BUTTON. Removed const that not needs for styling button.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,20 +2,9 @@ import React from 'react'
 import "./Button.scss"
 
 const Button = (props) => {
-  console.log(props);
-  // let btnStyle = "backgroundColor: pink;"
-  let btn = "btn"
-
-  // let className = 'menu';
-  // if (this.props.isActive) {
-  //   className += ' menu-active';
-  // }
-  // return <span className={className}>Menu</span>
-
+  console.log(props)
   return (
     <button
-      className={btn}
-      style={props.style}
       onClick={props.action}>
       {props.title}
     </button>)
