1db4b337d611c23cb7ae3e523e6f37bda48c9001
wongprom <jimmybackstrom@hotmail.com>
2019-07-15 19:02:40 +0200

    MEST-LASTA-ARTIKLAR. Removed outcomment code and white space.

--- src/components/Pages/MestLastaArtiklar/MestLastaArtiklar.js
+++ src/components/Pages/MestLastaArtiklar/MestLastaArtiklar.js
@@ -2,19 +2,15 @@
 import React from 'react';
 import Divider from '../../Divider/Divider';
 import BigArticleCard from '../../Card/BigCard/BigArticleCard';
-
 import './MestLastaArtiklar.scss';
-
 import { articles } from "../../../utilities/fakeData";
-const MestLastaArtiklar = (props) => {
 
+const MestLastaArtiklar = (props) => {
   const data = articles
   const header = 'Mest Lästa Artiklar';
-
   const fixedDivider = {
     className: "divider divider-fixed"
   }
-
   // if on page /MestLastaArtiklar, add fixed styling to component Divider.
   const divider = props.match.path === "/MestLastaArtiklar" && <Divider className={fixedDivider.className} header={header} />
 
