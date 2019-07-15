fdb9be476ba175d3f1a06e253a79b9fda8ad21be
wongprom <jimmybackstrom@hotmail.com>
2019-07-15 19:00:59 +0200

    KONTAKTA-OSS. Removed outcomment code and white space.

--- src/components/Pages/KontaktaOss/KontaktaOss.js
+++ src/components/Pages/KontaktaOss/KontaktaOss.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Divider from '../../Divider/Divider'
 import FormContainer from '../../FormContainer/FormContainer';
 import contactUs from '../../../images/contact-us.jpg';
-
 import './KontaktaOss.scss';
 
 const KontaktaOss = (props) => {
@@ -10,12 +9,11 @@ const KontaktaOss = (props) => {
   const fixedDivider = {
     className: "divider divider-fixed"
   }
-
+  // if URL contain "/KontaktaOss", make divider fixed.
   const divider = props.match.path === "/KontaktaOss" && <Divider className={fixedDivider.className} header={header} />
 
   console.log(props)
   return (
-
     <div className="kontaktaOss" >
       {divider}
       <div id="header-text">
@@ -27,10 +25,7 @@ const KontaktaOss = (props) => {
       <div className="formContainerWrapper">
         <FormContainer />
       </div>
-
     </div >
-
-
   )
 }
 
