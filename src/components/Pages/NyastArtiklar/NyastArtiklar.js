708de0cb2116f8fe50b3300b118e3c1e3603b6e4
wongprom <jimmybackstrom@hotmail.com>
2019-07-15 19:03:59 +0200

    NYAST-ARTIKLAR. Removed outcomment code and white space.

--- src/components/Pages/NyastArtiklar/NyastArtiklar.js
+++ src/components/Pages/NyastArtiklar/NyastArtiklar.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import Divider from '../../Divider/Divider';
 import BigArticleCard from '../../Card/BigCard/BigArticleCard';
-
 import './NyastArtiklar.scss';
-
 import { articles } from "../../../utilities/fakeData";
 
 
 
 const NyastArtiklar = (props) => {
-
   const data = articles
   const header = 'Nyast Artiklar';
-
   const fixedDivider = {
     className: "divider divider-fixed"
   }
-
   // if on page /NyastArtiklar, add fixed styling to component Divider.
   const divider = props.match.path === "/NyastArtiklar" && <Divider className={fixedDivider.className} header={header} />
 
