5afeb55d190dc44e4a9c8ad814909073bb549e40
wongprom <jimmybackstrom@hotmail.com>
2019-07-16 17:04:22 +0200

    Updates

---
+++ precache-manifest.433d526398242c1f1e93274fb5bc4669.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f709f4ac71ebedd529907f71b9c204",
+    "url": "/gutar/static/media/sunset-1920x1079jpg.31f709f4.jpg"
+  },
+  {
+    "revision": "068de75d3acb349fa57807bc2eb5f949",
+    "url": "/gutar/static/media/petterkvist.068de75d.jpg"
+  },
+  {
+    "revision": "174f022655fb8ff411afc9177ce027d9",
+    "url": "/gutar/static/media/gotlandsAngBatarAB.174f0226.jpg"
+  },
+  {
+    "revision": "cd8ee26dda545724f1170af2cf5f5813",
+    "url": "/gutar/static/media/contact-us.cd8ee26d.jpg"
+  },
+  {
+    "revision": "a17c8a46b39da5d2e401",
+    "url": "/gutar/static/js/runtime~main.a17c8a46.js"
+  },
+  {
+    "revision": "d1e6a06a4479f6d2dbce",
+    "url": "/gutar/static/js/main.d1e6a06a.chunk.js"
+  },
+  {
+    "revision": "55b8b771d65928e7c577",
+    "url": "/gutar/static/js/1.55b8b771.chunk.js"
+  },
+  {
+    "revision": "d1e6a06a4479f6d2dbce",
+    "url": "/gutar/static/css/main.bb7b0de5.chunk.css"
+  },
+  {
+    "revision": "35b7030445524c45d668b1cd0c1855ce",
+    "url": "/gutar/index.html"
+  }
+];
